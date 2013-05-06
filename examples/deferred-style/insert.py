20e3d175bc670285896392bfe62611d2b6cf792b
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-05-06 16:16:31 -0700

    Added a new working example of inserting data.
    (And checking for the results.)

---
+++ examples/deferred-style/insert.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+import sys
+
+from twisted.internet import defer, reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.python import log
+
+from txredis.client import RedisClientFactory
+
+
+def doInsert(client):
+    log.msg("Preparing to insert a bunch of data ...")
+    deferreds = []
+    for x in xrange(10000):
+        key = "key-%s" % x
+        value = datetime.now().isoformat()
+        d = client.set(key, value)
+        d.addErrback(log.err)
+        deferreds.append(d)
+    return defer.DeferredList(deferreds)
+
+
+def getResult(results):
+    log.msg("Getting result ...")
+    errors = 0
+    successes = 0
+    for deferredResult, serverResult in results:
+        if deferredResult and serverResult == "OK":
+            successes += 1
+        else:
+            errors += 1
+    log.msg("Data insertion results: %s success and %s errors" % (
+        successes, errors))
+
+
+def finish(ignore):
+    reactor.stop()
+
+
+def example():
+    endpoint = TCP4ClientEndpoint(reactor, "127.0.0.1", 6379)
+    d = endpoint.connect(RedisClientFactory())
+    d.addErrback(log.err)
+    d.addCallback(doInsert)
+    d.addCallback(getResult)
+    d.addErrback(log.err)
+    d.addCallback(finish)
+    return d
+
+
+if __name__ == "__main__":
+    log.startLogging(sys.stdout)
+    example()
+    reactor.run()
